bd1b90c36764d2d7c1ba605d3f27a46aac8dcbe1
LJosueCM <ljosuecm@gmail.com>
2022-05-03 16:09:40 -0500

    Añadidos endpoints para requerimientos

--- server.js
+++ server.js
@@ -54,6 +54,50 @@ app.delete('/explorers/:id', async (req, res) => {
 	return res.json({message: "Eliminado correctamente"});
 });
 
+app.get('/datos', async (req, res) => {
+    const allDatos =  await prisma.datos.findMany({});
+    res.json(allDatos);
+});
+
+app.get('/datos/:id', async (req, res) => {
+  const id = req.params.id;
+  const datos = await prisma.datos.findUnique({where: {id: parseInt(id)}});
+  res.json(datos);
+});
+
+app.post('/datos', async (req, res) => {
+    const datos = {
+      name: req.body.name,
+      lang: req.body.lang,
+      missionCommander: req.body.missionCommander,
+      enrollments: req.body.enrollments
+     };
+    const message = 'Datos creados';
+    await prisma.datos.create({data: datos});
+    return res.json({message});
+});
+
+app.put('/datos/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+
+	await prisma.datos.update({
+		where: {
+			id: id
+		},
+		data: {
+			missionCommander: req.body.missionCommander
+		}
+	})
+
+	return res.json({message: "Actualizado correctamente"});
+});
+
+app.delete('/datos/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+	await prisma.datos.delete({where: {id: id}});
+	return res.json({message: "Eliminado correctamente"});
+});
+
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
